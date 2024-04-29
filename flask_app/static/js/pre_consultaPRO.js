bab12d391eadd8a621e752747e28379b9affb52c
Hector Chacon <hector.chacong@gmail.com>
2024-04-29 19:30:12 -0400

    formulario PRO

--- flask_app/static/js/pre_consultaPRO.js
+++ flask_app/static/js/pre_consultaPRO.js
@@ -2,32 +2,62 @@ const formulario = document.getElementById('formulario');
 const inputs = document.querySelectorAll('#formulario input');
 
 const expresiones = {
-	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-	raza: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-	edad: /^.{4,12}$/, // 4 a 12 digitos.
-	peso: /^.{4,12}$/, // 4 a 12 digitos.
-	edad_adopcion: /^\d{7,14}$/, // 7 a 14 numeros.
-	adquisicion: /^\d{7,14}$/, // 7 a 14 numeros.
-	tiempo_madre: /^\d{7,14}$/, // 7 a 14 numeros.
-	edad_calle: /^\d{7,14}$/ // 7 a 14 numeros.
+	nombre_mas: /^.{2,255}$/, // todo tipo de caracteres
+	raza: /^.{3,255}$/, 
+	edad: /^.{4,255}$/, 
+	peso: /^.{2,255}$/, 
+	edad_adopcion: /^.{2,255}$/, 
+	donde_adquisicion: /^.{4,255}$/, 
+	tiempo_con_madre_hnos: /^.{2,255}$/, 
+	momento_salida_a_calle: /^.{2,255}$/, 
+
+	nom_fecha_ultim_vac: /^.{6,255}$/, 
+	ultimo_antipara: /^.{6,255}$/, 
+	fecha_castracion: /^.{2,255}$/, 
+	motivo_castracion: /^.{2,255}$/, 
+
+	tipo_alimentacion: /^.{6,255}$/, 
+	motivo_entrenamiento: /^.{2,255}$/, 
+
+	diagnostico: /^.{2,255}$/, 
+	problema_fisico: /^.{2,255}$/, 
+	medicamentos: /^.{2,255}$/, 
+
+	motivo_consulta: /^.{6,255}$/, 
+	otra_mascota: /^.{2,255}$/ 
 }
 
 const campos = {
-	nombre: true,
-	raza: true,
-	edad: true,
-	peso: true,
-	edad_adopcion: true,
-	adquisicion: true,
-	tiempo_madre: true,
-	edad_calle: true,
+	nombre_mas: false,
+	raza: false,
+	edad: false,
+	peso: false,
+	edad_adopcion: false,
+	donde_adquisicion: false,
+	tiempo_con_madre_hnos: false,
+	momento_salida_a_calle: false,
+
+	nom_fecha_ultim_vac: false,
+	ultimo_antipara: false,
+	fecha_castracion: false,
+	motivo_castracion: false,
+
+	tipo_alimentacion: false,
+	motivo_entrenamiento: false,
+
+	diagnostico: false,
+	problema_fisico: false,
+	medicamentos: false,
+
+	motivo_consulta: false,
+	otra_mascota: false,
 }
 
 const validarFormulario = (e) => {
 	switch (e.target.name) {
-		case "nombre":
-			console.log('se ejecuto nombre');
-			validarCampo(expresiones.nombre, e.target, 'nombre');
+		case "nombre_mas":
+			console.log('se ejecuto nombre_mas');
+			validarCampo(expresiones.nombre_mas, e.target, 'nombre_mas');
 		break;
 		case "raza":
 			console.log('se ejecutp raza');
@@ -40,23 +70,66 @@ const validarFormulario = (e) => {
 		case "peso":
 			console.log('se ejecutp peso');
 			validarCampo(expresiones.peso, e.target, 'peso');
-
 		break;
 		case "edad_adopcion":
 			console.log('se ejecuto edad_adopcion');
 			validarCampo(expresiones.edad_adopcion, e.target, 'edad_adopcion');
 		break;
-		case "adquisicion":
-			console.log('se ejecuto adquisicion');
-			validarCampo(expresiones.adquisicion, e.target, 'adquisicion');
+		case "donde_adquisicion":
+			console.log('se ejecuto donde_adquisicion');
+			validarCampo(expresiones.donde_adquisicion, e.target, 'donde_adquisicion');
 		break;
-		case "tiempo_madre":
-			console.log('se ejecuto tiempo_madre');
-			validarCampo(expresiones.tiempo_madre, e.target, 'tiempo_madre');
+		case "tiempo_con_madre_hnos":
+			console.log('se ejecuto tiempo_con_madre_hnos');
+			validarCampo(expresiones.tiempo_con_madre_hnos, e.target, 'tiempo_con_madre_hnos');
 		break;
-		case "edad_calle":
-			console.log('se ejecuto edad_calle');
-			validarCampo(expresiones.edad_calle, e.target, 'edad_calle');
+		case "momento_salida_a_calle":
+			console.log('se ejecuto momento_salida_a_calle');
+			validarCampo(expresiones.momento_salida_a_calle, e.target, 'momento_salida_a_calle');
+		break;
+		case "nom_fecha_ultim_vac":
+			console.log('se ejecuto nom_fecha_ultim_vac');
+			validarCampo(expresiones.nom_fecha_ultim_vac, e.target, 'nom_fecha_ultim_vac');
+		break;
+		case "ultimo_antipara":
+			console.log('se ejecuto ultimo_antipara');
+			validarCampo(expresiones.ultimo_antipara, e.target, 'ultimo_antipara');
+		break;
+		case "fecha_castracion":
+			console.log('se ejecuto fecha_castracion');
+			validarCampo(expresiones.fecha_castracion, e.target, 'fecha_castracion');
+		break;
+		case "motivo_castracion":
+			console.log('se ejecuto motivo_castracion');
+			validarCampo(expresiones.motivo_castracion, e.target, 'motivo_castracion');
+		break;
+		case "tipo_alimentacion":
+			console.log('se ejecuto tipo_alimentacion');
+			validarCampo(expresiones.tipo_alimentacion, e.target, 'tipo_alimentacion');
+		break;
+		case "motivo_entrenamiento":
+			console.log('se ejecuto motivo_entrenamiento');
+			validarCampo(expresiones.motivo_entrenamiento, e.target, 'motivo_entrenamiento');
+		break;
+		case "diagnostico":
+			console.log('se ejecuto diagnostico');
+			validarCampo(expresiones.diagnostico, e.target, 'diagnostico');
+		break;
+		case "problema_fisico":
+			console.log('se ejecuto problema_fisico');
+			validarCampo(expresiones.problema_fisico, e.target, 'problema_fisico');
+		break;
+		case "medicamentos":
+			console.log('se ejecuto medicamentos');
+			validarCampo(expresiones.medicamentos, e.target, 'medicamentos');
+		break;
+		case "motivo_consulta":
+			console.log('se ejecuto motivo_consulta');
+			validarCampo(expresiones.motivo_consulta, e.target, 'motivo_consulta');
+		break;
+		case "otra_mascota":
+			console.log('se ejecuto otra_mascota');
+			validarCampo(expresiones.otra_mascota, e.target, 'otra_mascota');
 		break;
 	}
 
@@ -107,8 +180,8 @@ let formStepsNum = 0;
 nextBtn1.addEventListener('click', (event) => {
     event.preventDefault(); // Evita que el formulario se envíe automáticamente
 
-	// guarda el valor de la fecha de nacimiento para usarla en el if
-	const fechaNacimiento = document.getElementById('nacimiento').value;
+	// guarda el valor de la fecha de fecha_nac para usarla en el if
+	const fechaNacimiento = document.getElementById('fecha_nac').value;
 
 	// se le da valor de true or false para usar en el if
 	const radioButtons = document.querySelectorAll('input[name="dog_or_cat"]');
@@ -119,7 +192,7 @@ nextBtn1.addEventListener('click', (event) => {
         }
     });
 
-	// se le da valor de true or false para usar en el if
+	// se le da valor de true or false al sexo para usar en el if
 	const radioButtons2 = document.querySelectorAll('input[name="sexo"]');
     let sexoSelected = false;
     radioButtons2.forEach(button => {
@@ -130,14 +203,14 @@ nextBtn1.addEventListener('click', (event) => {
 
 
     // Verifica si solo los campos sean verdaderos
-    if (campos.nombre && 
+    if (campos.nombre_mas && 
 		campos.raza && 
 		campos.edad && 
 		campos.peso && 
 		campos.edad_adopcion && 
-		campos.adquisicion && 
-		campos.tiempo_madre && 
-		campos.edad_calle && 
+		campos.donde_adquisicion && 
+		campos.tiempo_con_madre_hnos && 
+		campos.momento_salida_a_calle && 
 		dogOrCatSelected && 
 		fechaNacimiento !== "" && 
 		sexoSelected) {
@@ -159,8 +232,18 @@ nextBtn1.addEventListener('click', (event) => {
 nextBtn2.addEventListener('click', (event) => {
     event.preventDefault(); // Evita que el formulario se envíe automáticamente
 
-    // Verifica si solo los campos nombre y apellido son verdaderos
-    if (campos.nombre && campos.raza) {
+    // se le da valor de true or false a castracion para usar en el if
+	const radioButtons = document.querySelectorAll('input[name="castracion"]');
+    let castracionSelected = false;
+    radioButtons.forEach(button => {
+        if (button.checked) {
+            castracionSelected = true;
+        }
+    });
+
+
+    // Verifica si solo los campos nombre_mas y apellido son verdaderos
+    if (campos.nom_fecha_ultim_vac && campos.ultimo_antipara && castracionSelected ) {
         // Incrementa el número de pasos del formulario
         formStepsNum++;
         
@@ -172,6 +255,7 @@ nextBtn2.addEventListener('click', (event) => {
         console.log("boton escuchado 2  jaja");
     } else {
         console.log("boton2 escuchado hay false jaja");
+        document.getElementById('formulario__mensaje2').classList.add('formulario__mensaje-activo');
     }
 });
 
@@ -179,8 +263,20 @@ nextBtn2.addEventListener('click', (event) => {
 nextBtn3.addEventListener('click', (event) => {
     event.preventDefault(); // Evita que el formulario se envíe automáticamente
 
-    // Verifica si solo los campos nombre y apellido son verdaderos
-    if (campos.nombre && campos.raza) {
+    // se le da valor de true or false a tipo_alimentacion para usar en el if
+	const radioButtons = document.querySelectorAll('input[name="tuvo_entrenamiento"]');
+    let entrenamientoSelected = false;
+    radioButtons.forEach(button => {
+        if (button.checked) {
+            entrenamientoSelected = true;
+        }
+    });
+
+
+
+
+    // Verifica si solo los campos nombre_mas y apellido son verdaderos
+    if (campos.tipo_alimentacion && entrenamientoSelected) {
         // Incrementa el número de pasos del formulario
         formStepsNum++;
         
@@ -192,14 +288,15 @@ nextBtn3.addEventListener('click', (event) => {
         console.log("boton escuchado 3  jaja");
     } else {
         console.log("boton3 escuchado hay false jaja");
+        document.getElementById('formulario__mensaje3').classList.add('formulario__mensaje-activo');
     }
 });
 
 nextBtn4.addEventListener('click', (event) => {
     event.preventDefault(); // Evita que el formulario se envíe automáticamente
 
-    // Verifica si solo los campos nombre y apellido son verdaderos
-    if (campos.nombre && campos.raza) {
+    // Verifica si solo los campos nombre_mas y apellido son verdaderos
+    if (campos.diagnostico && campos.problema_fisico && campos.medicamentos) {
         // Incrementa el número de pasos del formulario
         formStepsNum++;
         
@@ -211,14 +308,15 @@ nextBtn4.addEventListener('click', (event) => {
         console.log("boton escuchado 4  jaja");
     } else {
         console.log("boton4 escuchado hay false jaja");
+        document.getElementById('formulario__mensaje4').classList.add('formulario__mensaje-activo');
     }
 });
 
 submitBtn.addEventListener('click', (event) => {
     event.preventDefault(); // Evita que el formulario se envíe automáticamente
 
-    // Verifica si solo los campos nombre y apellido son verdaderos
-    if (campos.nombre && campos.raza) {
+    // Verifica si solo los campos nombre_mas y apellido son verdaderos
+    if (campos.motivo_consulta && campos.otra_mascota) {
         // Incrementa el número de pasos del formulario
         formStepsNum++;
         
@@ -230,6 +328,7 @@ submitBtn.addEventListener('click', (event) => {
         console.log("boton escuchado 5  jaja");
     } else {
         console.log("boton5 escuchado hay false jaja");
+        document.getElementById('formulario__mensaje5').classList.add('formulario__mensaje-activo');
     }
 });
 
