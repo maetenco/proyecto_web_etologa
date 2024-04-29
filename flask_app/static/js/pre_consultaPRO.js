a2ee7c40f75415466dab3df2377e652afb10a5d2
Hector Chacon <hector.chacong@gmail.com>
2024-04-29 14:47:09 -0400

    formulario delgado

--- flask_app/static/js/pre_consultaPRO.js
+++ flask_app/static/js/pre_consultaPRO.js
@@ -82,26 +82,7 @@ const validarCampo = (expresion, input, campo) => {
 	}
 }
 
-const validarPassword2 = () => {
-	const inputPassword1 = document.getElementById('password');
-	const inputPassword2 = document.getElementById('password2');
 
-	if(inputPassword1.value !== inputPassword2.value){
-		document.getElementById(`grupo__password2`).classList.add('formulario__grupo-incorrecto');
-		document.getElementById(`grupo__password2`).classList.remove('formulario__grupo-correcto');
-		document.querySelector(`#grupo__password2 i`).classList.add('fa-times-circle');
-		document.querySelector(`#grupo__password2 i`).classList.remove('fa-check-circle');
-		document.querySelector(`#grupo__password2 .formulario__input-error`).classList.add('formulario__input-error-activo');
-		campos['password'] = false;
-	} else {
-		document.getElementById(`grupo__password2`).classList.remove('formulario__grupo-incorrecto');
-		document.getElementById(`grupo__password2`).classList.add('formulario__grupo-correcto');
-		document.querySelector(`#grupo__password2 i`).classList.remove('fa-times-circle');
-		document.querySelector(`#grupo__password2 i`).classList.add('fa-check-circle');
-		document.querySelector(`#grupo__password2 .formulario__input-error`).classList.remove('formulario__input-error-activo');
-		campos['password'] = true;
-	}
-}
 
 inputs.forEach((input) => {
 	input.addEventListener('keyup', validarFormulario);
@@ -112,7 +93,6 @@ inputs.forEach((input) => {
 
 
 const prevBtns = document.querySelectorAll('.btn-pre')
-/* const nextBtns = document.querySelectorAll('.btn-next')*/
 const nextBtn1 = document.getElementById('btn-next1')
 const nextBtn2 = document.getElementById('btn-next2')
 const nextBtn3 = document.getElementById('btn-next3')
