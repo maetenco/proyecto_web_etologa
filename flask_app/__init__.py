a517e7a5e22c7bd56d22b38297ac34f6e310faed
Marianela Encina <marianela.encina@outlook.es>
2024-05-01 19:23:57 -0400

    actualizacion desde mi lado xd

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -6,3 +6,6 @@ app = Flask(__name__)
 
 #llave secreta
 app.secret_key = "llavesita secret top"
+
+app.config['UPLOAD_FOLDER'] = "flask_app/static/archivo/examenes"
+app.config['UPLOAD_FOLDER2'] = "flask_app/static/archivo/derivaciones"
